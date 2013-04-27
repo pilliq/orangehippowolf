07442e8b79e043fae1de29e0fccc01169a7347b3
Phillip Quiza <pquiza@gmail.com>
2013-04-27 14:58:27 -0400

    Fixed a small bug in student model

--- application/models/student.php
+++ application/models/student.php
@@ -5,6 +5,9 @@ class Student extends User {
     public static function create($data) {
 	$data['instructor'] = 0;
 	User::create($data);
+
+	$ruid = $data['ruid'];
+	$username = $data['username'];
 	DB::query("INSERT INTO student(ruid,netid) values('$ruid','$username');");
     }
 }
