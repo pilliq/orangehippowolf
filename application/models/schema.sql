7f0e31ce32e1f26567bd9bfe7e39600bd4ee56c3
Phillip Quiza <aznwhtey@gmail.com>
2013-04-27 13:47:23 -0400

    Added SQL schema

---
+++ application/models/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS instructor;
+
+CREATE TABLE user (
+    username varchar(127) primary key, /* netid */
+    password char(127),
+    email varchar(127),
+    first_name varchar(255),
+    last_name varchar(255),
+    instructor bit(1),
+    created timestamp not null default CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE student (
+    ruid int(11) unsigned primary key,
+    netid varchar(127) references user,
+    gradDate date,
+    majorCreditsCompleted int(11),
+    creditsCompleted int(11),
+    gpa decimal(10,3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE instructor (
+    tid int(11) unsigned NOT NULL AUTO_INCREMENT primary key,
+    netid varchar(127) references user,
+    department varchar(127)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
