bfb7d7c98783830c30584d6e30fd4602a7d95fe7
Phillip Quiza <aznwhtey@gmail.com>
2013-05-07 00:54:31 -0400

    Added a few more tables

--- application/models/schema.sql
+++ application/models/schema.sql
@@ -1,6 +1,11 @@
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS instructor;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS building;
+DROP TABLE IF EXISTS course_offering;
+DROP TABLE IF EXISTS request;
+DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
     username varchar(127) primary key, /* netid */
@@ -13,11 +18,12 @@ CREATE TABLE user (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE student (
-    ruid int(11) unsigned primary key,
+    ruid char(11) primary key,
     netid varchar(127) references user,
-    gradDate date,
-    majorCreditsCompleted int(11),
-    creditsCompleted int(11),
+    gradMonth varchar(127),
+    gradYear varchar(127),
+    majorCredits int(11),
+    credits int(11),
     gpa decimal(10,3)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -25,4 +31,50 @@ CREATE TABLE instructor (
     tid int(11) unsigned NOT NULL AUTO_INCREMENT primary key,
     netid varchar(127) references user,
     department varchar(127)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE course (
+    cid varchar(127) primary key,
+    unit varchar(127),
+    subject varchar(127),
+    course_number varchar(127),
+    title varchar(127),
+    credits int(11)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE course_offering (
+    cid varchar(127),
+    year varchar(127),
+    term varchar(127),
+    section varchar(127),
+    instructor varchar(127),
+    foreign key (cid) references course(cid),
+    primary key (cid,section)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE building (
+    code varchar(127) primary key,
+    name varchar(127),
+    campus varchar(127)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE room (
+    number varchar(127),
+    building varchar(127) references building,
+    capacity int(11)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* 
+CREATE TABLE registration (
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+*/
+
+CREATE TABLE request (
+    rid int(11) unsigned NOT NULL AUTO_INCREMENT primary key,
+    student varchar(127),
+    course varchar(127),
+    status varchar(127),
+    created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (student) REFERENCES user(username),
+    FOREIGN KEY (course) REFERENCES course(cid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
