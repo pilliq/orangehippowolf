229dce99e275a364d3a2889c5272972bda6aa0a3
Phillip Quiza <pquiza@gmail.com>
2013-05-09 18:03:21 -0400

    Added some things to student requests view

--- application/views/student/home.blade.php
+++ application/views/student/home.blade.php
@@ -9,45 +9,48 @@
 @endsection
 
 @section('content')
-    @if (Session::has('sucess'))
-	<div class="alert alert-success">
-	    {{ Session::get('success') }}
-	</div>
-    @endif
     <div class="page-header">
 	    <h1>
 		Requests
+		<a class="btn btn-primary pull-right" href="{{ URL::to_route('create_requests') }}"><i class="icon-plus icon-white"></i> Create a new request</a> 
 	    </h1>
     </div>
-    <a href="{{ URL::to_route('create_requests') }}" class="btn btn-primary">Create a new request</a>
-    <table class="table table-hover">
-    <thead>
-	<tr>
-	    <th>Course ID</th>
-	    <th>Course</th>
-	    <th>Status</th>
-	</tr>
-    </thead>
-    <tbody>
-	@foreach ($requests as $request)
+    @if (count($requests) == 0)
+	<h2>You don't have any requests!</h2>
+    @else
+	<table class="table table-hover">
+	<thead>
 	    <tr>
-	    <td>{{ $request->course }}</td>
-	    <td>{{ Course::get_name($request->course) }}</td>
-	    <td>
-		@if ($request->status == 'granted')
-		    <span class="label label-success">Granted</span>
-		@elseif ( $request->status == 'denied') 
-		    <span class="label label-important">Denied</span>
-		@elseif ( $request->status == 'inactive') 
-		    <span class="label">Inactive</span>
-		@elseif ( $request->status == 'pending') 
-		    <span class="label label-warning">Pending</span>
-		@elseif ( $request->status == 'expired') 
-		    <span class="label label-inverse">Expired</span>
-		@endif
-	    </td>
+		<th>Date Requested</th>
+		<th>Course ID</th>
+		<th>Section</th>
+		<th>Course</th>
+		<th>Status</th>
 	    </tr>
-	@endforeach
-    </tbody>
-    </table>
+	</thead>
+	<tbody>
+	    @foreach ($requests as $request)
+		<tr>
+		<td>{{ $request->created }}</td>
+		<td>{{ $request->course }}</td>
+		<td>{{ $request->section }}</td>
+		<td>{{ Course::get_name($request->course) }}</td>
+		<td>
+		    @if ($request->status == 'granted')
+			<span class="label label-success">Granted</span>
+		    @elseif ( $request->status == 'denied') 
+			<span class="label label-important">Denied</span>
+		    @elseif ( $request->status == 'inactive') 
+			<span class="label">Inactive</span>
+		    @elseif ( $request->status == 'pending') 
+			<span class="label label-warning">Pending</span>
+		    @elseif ( $request->status == 'expired') 
+			<span class="label label-inverse">Expired</span>
+		    @endif
+		</td>
+		</tr>
+	    @endforeach
+	</tbody>
+	</table>
+    @endif
 @endsection
